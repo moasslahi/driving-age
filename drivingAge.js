7167b468b4765cea1321674198f325aa34126d52
Mohammed Asslahi <asslahi37@gmail.com>
2019-07-06 23:10:53 +0300

    driving age

---
+++ drivingAge.js
@@ -0,0 +1,22 @@
+var age = prompt("What is your age?");
+
+if (Number(age) < 18) {
+	alert("Sorry, you are too yound to drive this car. Powering off");
+} else if (Number(age) > 18) {
+	alert("Powering On. Enjoy the ride!");
+} else if (Number(age) === 18) {
+	alert("Congratulations on your first year of driving. Enjoy the ride!");
+}
+
+
+// improved version with functions
+
+function checkDriverAge(age) { // function takes age parameter
+	if (Number(age) < 18) {
+		return "Sorry, you are too yound to drive this car. Powering off";
+	} else if (Number(age) > 18) {
+		return "Powering On. Enjoy the ride!";
+	} else if (Number(age) === 18) {
+		return "Congratulations on your first year of driving. Enjoy the ride!";
+	}
+}
